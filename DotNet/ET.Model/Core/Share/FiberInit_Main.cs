7bb9ecda78702acc4bd9b31ca9f93b6a2cdfa507
ZZQ <zhiqiangz@opera.com>
2025-01-03 11:29:57 +0800

    修改目录

---
+++ DotNet/ET.Model/Core/Share/FiberInit_Main.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ET
+{
+    [Invoke(SceneType.Main)]
+    public class FiberInit_Main: AInvokeHandler<FiberInit, ETTask>
+    {
+        public override async ETTask Handle(FiberInit fiberInit)
+        {
+            Scene root = fiberInit.Fiber.Root;
+            
+            int sceneType = SceneTypeSingleton.Instance.GetSceneType(Options.Instance.SceneName);
+            root.SceneType = sceneType;
+            Log.Info($"FiberInit_Main {root.SceneType}  {root.Name}");
+            await EventSystem.Instance.PublishAsync(root, new EntryEvent1());
+            await EventSystem.Instance.PublishAsync(root, new EntryEvent2());
+            await EventSystem.Instance.PublishAsync(root, new EntryEvent3());
+        }
+    }
+}

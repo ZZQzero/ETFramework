7ef3fa9e13b2b4948da70841536153858017e0aa
ZZQ <zhiqiangz@opera.com>
2025-01-03 10:48:12 +0800

    添加项目，修改目录结构

--- DotNet/ET.Loader/obj/Debug/net8.0/ET.Loader.AssemblyInfo.cs
+++ DotNet/ET.Loader/obj/Debug/net8.0/ET.Loader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ET.Loader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d493d18ae14bd41ac915a3edb72af98b19a3022")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dea2766aa4d52936c29c6c697a505e003b4f0fe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ET.Loader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ET.Loader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

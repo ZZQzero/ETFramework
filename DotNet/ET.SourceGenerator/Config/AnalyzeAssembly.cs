c42296609bdcdb7cd31cf95cce4f9f9c5a1b7df9
ZZQ <zhiqiangz@opera.com>
2025-01-06 18:32:06 +0800

    修改代码生成

--- DotNet/ET.SourceGenerator/Config/AnalyzeAssembly.cs
+++ DotNet/ET.SourceGenerator/Config/AnalyzeAssembly.cs
@@ -6,33 +6,37 @@ namespace ET
     {
         private const string Core = "ET.Core";
         private const string Model = "ET.Model";
+        private const string ClientModel = "ETClient.Model";
         private const string Hotfix = "ET.Hotfix";
+        private const string ClientHotfix = "ETClient.Hotfix";
         private const string ModelView = "ET.ModelView";
+        private const string ClientModelView = "ETClient.ModelView";
         private const string HotfixView = "ET.HotfixView";
+        private const string ClientHotfixView = "ETClient.HotfixView";
 
         public static readonly string[] AllHotfix =
         [
-            Hotfix, HotfixView
+            Hotfix, HotfixView,ClientHotfix,ClientHotfixView
         ];
 
         public static readonly string[] AllModel =
         [
-            Model, ModelView
+            Model, ModelView, ClientModel, ClientModelView
         ];
 
         public static readonly string[] AllModelHotfix =
         [
-            Model, Hotfix, ModelView, HotfixView
+            Model, Hotfix, ModelView, HotfixView, ClientModel, ClientHotfix, ClientModelView, ClientHotfixView
         ];
         
         public static readonly string[] All =
         [
-            Core, Model, Hotfix, ModelView, HotfixView
+            Core, Model, Hotfix, ModelView, HotfixView, ClientModel, ClientHotfix, ClientModelView, ClientHotfixView
         ];
         
         public static readonly string[] AllLogicModel =
         [
-            Model
+            Model,ClientModel
         ];
     }
     
